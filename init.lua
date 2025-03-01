d5d0c215e551643eccc18be8d8536f6d9c757a22
vuongkienthanh <thanhstardust@outlook.com>
2025-03-01 12:43:00 +0700

    update lazy setup

--- init.lua
+++ init.lua
@@ -16,4 +16,10 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+    checker = { enabled = false },
+    change_detection = { enabled = false },
+})
